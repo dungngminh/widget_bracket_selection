12cbcc4dc744b86140a5a34a4a1f628313dbed4a
Kreazy <79961692+kreazyme@users.noreply.github.com>
2024-05-30 14:41:22 +0700

    [Feat] Support private object (#1)

--- src/bracketUtil.ts
+++ src/bracketUtil.ts
@@ -1,4 +1,5 @@
 "use strict";
+import * as vscode from 'vscode';
 
 export namespace bracketUtil {
   let bracketParis = [
@@ -6,19 +7,24 @@ export namespace bracketUtil {
     ["[", "]"],
   ];
 
-  export function isMatch(open: string, close: string): Boolean {
+  export function isMatch(open: string, close: string): boolean {
     return bracketParis.findIndex((p) => p[0] === open && p[1] === close) >= 0;
   }
 
-  export function isOpenBracket(char: string): Boolean {
+  export function isOpenBracket(char: string): boolean {
     return bracketParis.findIndex((pair) => pair[0] === char) >= 0;
   }
 
-  export function isCloseBracket(char: string): Boolean {
+  export function isCloseBracket(char: string): boolean {
     return bracketParis.findIndex((pair) => pair[1] === char) >= 0;
   }
 
-  export function isLetter(char: String) {
+  export function isLetter(char: string) {
     return char.length === 1 && char.match(/[a-z.]/i);
+  }
+
+  // check if private in dart
+  export function isUnderDashInDart(char: string) {
+    return char.length === 1 && char === '_' && vscode.window.activeTextEditor?.document.languageId === 'dart';
   }
 }
